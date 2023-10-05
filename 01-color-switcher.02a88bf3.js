d1c429b3f5717b2695b51379336237ae61fbf088
Andreykeyr <127974278+Andreykeyr@users.noreply.github.com>
2023-10-05 01:03:10 +0000

    Deploying to gh-pages from @ Andreykeyr/goit-js-hw-09@763e88b1eaee39a308162e9b59932064d965664f 🚀

---
+++ 01-color-switcher.02a88bf3.js
@@ -0,0 +1,3 @@
+!function(){let e=document.body,t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",l=>{a=setInterval(()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3),t.disabled=!0,d.disabled=!1}),d.addEventListener("click",e=>{clearInterval(a),d.disabled=!0,t.disabled=!1})}();//# sourceMappingURL=01-color-switcher.02a88bf3.js.map
+
+//# sourceMappingURL=01-color-switcher.02a88bf3.js.map
