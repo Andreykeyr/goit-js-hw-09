5e5ceead84165a8d5efd4a84e0ae4f5c4ea73f96
Andriy Razumnykh <127974278+Andreykeyr@users.noreply.github.com>
2023-10-05 03:51:40 +0300

    Initial commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = galleryItems
+  .map(image =>
+    `<li class="gallery__item">
+  <a class="gallery__link"
+  href="${image.original}">
+  <img class="gallery__image"
+  src="${image.preview}"
+  alt="${image.description}" />
+  </a>
+
+</li>`)
+  .join("");
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+}) 
+
+
+
+
+
